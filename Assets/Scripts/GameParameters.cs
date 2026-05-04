63ea7d19c3623680ff976fbba299dd953cc4c2c1
Jink2024 <Pausch@chapman.edu>
2026-05-04 12:49:12 -0700

    Confirmed bird movement, changed Move Speed to 10 in game parameters

--- Assets/Scripts/GameParameters.cs
+++ Assets/Scripts/GameParameters.cs
@@ -5,5 +5,5 @@ public static class GameParameters
     public static int NumberBirdsOnScreen = 1;
     public static float BirdsMinimumSecondsToWait = 3f;
     public static float BirdsMaximumSecondsToWait = 5f;
-    public static float BirdMovementSpeed = 1f;
+    public static float BirdMovementSpeed = 10f;
 }
