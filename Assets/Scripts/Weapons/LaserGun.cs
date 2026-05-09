0ed99df30c557ee5c5716ce01786b7f45cdb372e
PilouPilouPilou <pierrelouis.paget@gmail.com>
2026-05-09 15:13:35 -0700

    Files I forgot in my last commit.

--- Assets/Scripts/Weapons/LaserGun.cs
+++ Assets/Scripts/Weapons/LaserGun.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class LaserGun : Weapon
 {
@@ -6,5 +7,15 @@ public class LaserGun : Weapon
     protected override void PlayShootSound()
     {
         sounds.PlayLaserSound();
+    }
+    
+    protected override void PlayRecoil()
+    {
+        if (weaponVisual == null)
+            return;
+
+        weaponVisual
+            .DOPunchPosition(-weaponVisual.up * 0.3f, 0.05f)
+            .SetEase(Ease.OutBack);
     }
 }
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using DG.Tweening;
 
 public abstract class Weapon : MonoBehaviour
 {
     [SerializeField] protected GameObject projectilePrefab;
     [SerializeField] protected Transform projectileSpawnPoint;
+    [SerializeField] protected Transform weaponVisual;
+
     protected Sounds sounds;
 
     public virtual float FireCooldown => GameParameters.BulletFireCooldown;
@@ -15,7 +18,8 @@ public abstract class Weapon : MonoBehaviour
     public virtual void Shoot(Vector2 aimDirection)
     {
         PlayShootSound();
-
+        PlayRecoil();
+        
         GameObject projectileObject =
             Instantiate(
                 projectilePrefab,
@@ -41,5 +45,15 @@ public abstract class Weapon : MonoBehaviour
     
     protected virtual void PlayShootSound()
     {
+    }
+    
+    protected virtual void PlayRecoil()
+    {
+        if (weaponVisual == null)
+            return;
+
+        weaponVisual
+            .DOPunchPosition(-weaponVisual.up * 0.2f, 0.1f)
+            .SetEase(Ease.OutBack);
     }
 }
